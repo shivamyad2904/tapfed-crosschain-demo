e4acc66e03109e834c6e9a75b23f0a795603557b
Shivam Yadav <shivam.yadav24m@iiitg.ac.in>
2025-11-20 05:00:21 +0530

    Initial commit: TAPFed cross-chain demo scaffold

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+﻿import "@nomiclabs/hardhat-ethers";
+
+export default {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: { chainId: 1337 },
+    chainA: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337
+    },
+    chainB: {
+      url: "http://127.0.0.1:8546",
+      chainId: 1338
+    }
+  }
+};
---
+++ scripts/deploy_contracts.js
@@ -0,0 +1,18 @@
+﻿import hre from "hardhat";
+
+async function main() {
+  const DKG = await hre.ethers.getContractFactory("DKGRegistry");
+  const dkg = await DKG.deploy();
+  await dkg.deployed();
+  console.log("DKGRegistry deployed to:", dkg.address);
+
+  const CS = await hre.ethers.getContractFactory("CipherStore");
+  const cs = await CS.deploy();
+  await cs.deployed();
+  console.log("CipherStore deployed to:", cs.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
